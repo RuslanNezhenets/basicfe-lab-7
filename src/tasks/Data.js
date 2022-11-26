9d8e8261eb6e7354603b1de70b7bc9abde2c998c
RuslanNezhenets <ruslaneshenets@gmail.com>
2022-11-26 19:19:14 +0200

    add data

---
+++ src/tasks/Data.js
@@ -0,0 +1,32 @@
+export const Data = [
+    {
+        img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/306130/capsule_616x353.jpg?t=1667312669',
+        name: 'The Elder Scrolls Online',
+        price: '599₴',
+    },
+    {
+        img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1446780/capsule_616x353.jpg?t=1669252485',
+        name: 'Monster Hunter Rise',
+        price: '1399₴',
+    },
+    {
+        img: 'https://cdn.akamai.steamstatic.com/steam/apps/1286680/capsule_616x353.jpg?t=1660273090',
+        name: 'Tiny Tina\'s Wonderlands',
+        price: '1249₴',
+    },
+    {
+        img: 'https://cdn.akamai.steamstatic.com/steamcommunity/public/images/clans/33459075/abbd6b89dafb1dfc32f5ff8012cf687b1f0139db.jpg',
+        name: 'Sekiro™: Shadows Die Twice - GOTY Edition',
+        price: '1608₴',
+    },
+    {
+        img: 'https://i.ytimg.com/vi/DuvN4YcJ0ZA/maxresdefault.jpg',
+        name: 'Metro Exodus',
+        price: '506₴',
+    },
+    {
+        img: 'https://cdn.akamai.steamstatic.com/steam/apps/570/capsule_616x353.jpg?t=1650611880',
+        name: 'Dota 2',
+        price: 'Бесплатно',
+    },
+]
